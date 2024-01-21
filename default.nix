da98e1519087bd89edf063e0a77ef87fc6d117d2
hitsmaxft <mfthits@gmail.com>
2024-01-21 13:37:21 +0800

    Update Python packages in default.nix

--- default.nix
+++ default.nix
@@ -3,10 +3,24 @@
   python = pkgs.python311;
   overrides = poetry2nix.defaultPoetryOverrides.extend (self:
     super: {
-      inherit (pkgs.python311Packages) grpcio;
-      inherit (pkgs.python311Packages) grpcio-status;
       inherit (pkgs.python311Packages) googleapis-common-protos;
-      inherit (pkgs.python311Packages) protobuf;
+      inherit (pkgs.python311Packages) 
+      cachetools             
+      certifi                      
+      charset-normalizer           
+      grpcio                       
+      grpcio-status                
+      idna                         
+      proto-plus                   
+      protobuf                     
+      pyasn1                       
+      pyasn1-modules               
+      requests                     
+      rsa                          
+      toml                         
+      tqdm                         
+      typing-extensions            
+      urllib3;
 
       google-ai-generativelanguage = super.google-ai-generativelanguage.overridePythonAttrs (
         old: {
